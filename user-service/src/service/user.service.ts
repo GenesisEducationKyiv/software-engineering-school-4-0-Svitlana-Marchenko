5acc0a20e82d0aa6a77a86eae7735d60792a2a94
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-07-01 22:17:49 +0300

    feature: add user-service microservice

---
+++ user-service/src/service/user.service.ts
@@ -0,0 +1,34 @@
+import { User } from '../data-access/entity/user.entity'
+import UserAlreadyExistError from '../error/types/userAlreadyExist.error'
+import {IUserService} from "./user.service.interface";
+import {IUserRepository} from "../data-access/repositories/user.repository.interface";
+import userRepository from "../data-access/repositories/user.repository";
+
+
+export class UserService implements IUserService{
+
+    constructor(private userRepository: IUserRepository) {}
+
+    async subscribeEmail(email: string): Promise<User> {
+        const user = await this.userRepository.getByEmail(email)
+
+        if (user) {
+            throw new UserAlreadyExistError({message: `User with email ${email} has been added before`, logging: true});
+        }
+      
+        try {
+            return await this.userRepository.saveByEmail(email)
+        } catch (error) {
+            throw new Error('Error creating user: ' + (error as Error).message)
+        }
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        try {
+            return await this.userRepository.getAll()
+        } catch (error) {
+            throw new Error('Error getting all users email: ' + (error as Error).message)
+        }
+    }
+}
+export default new UserService(userRepository)
